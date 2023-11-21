7141b6ab3b595af3e7d0a4dcd872502f6c9ba1a6
ppedvLukasK <lukask@ppedv.de>
2023-11-21 15:33:03 +0100

    M000

--- M000/Program.cs
+++ M000/Program.cs
@@ -1,1 +1,49 @@
-﻿
+﻿while (true)
+{
+	Console.Write("Gib die erste Zahl ein: ");
+	double zahl1 = double.Parse(Console.ReadLine());
+
+	Console.Write("Gib die zweite Zahl ein: ");
+	double zahl2 = double.Parse(Console.ReadLine());
+
+	foreach (Operation operation in Enum.GetValues<Operation>())
+	{
+		Console.WriteLine($"{(int) operation}: {operation}");
+	}
+
+	Console.Write("Gib eine Rechenoperation ein: ");
+	Operation op = Enum.Parse<Operation>(Console.ReadLine());
+
+	switch (op)
+	{
+		case Operation.Addition:
+			Console.WriteLine($"Ergebnis: {zahl1 + zahl2}");
+			break;
+		case Operation.Subtraktion:
+			Console.WriteLine($"Ergebnis: {zahl1 - zahl2}");
+			break;
+		case Operation.Multiplikation:
+			Console.WriteLine($"Ergebnis: {zahl1 * zahl2}");
+			break;
+		case Operation.Division:
+			if (zahl2 != 0)
+				Console.WriteLine($"Ergebnis: {zahl1 / zahl2}");
+			else
+				Console.WriteLine("Teilung durch 0 ist nicht möglich!");
+			break;
+		default:
+			Console.WriteLine("Fehler");
+			break;
+	}
+
+    Console.WriteLine("Enter drücken zum Wiederholen");
+	if (Console.ReadKey(true).Key != ConsoleKey.Enter)
+		break;
+	Console.Clear();
+}
+
+
+enum Operation
+{
+	Addition = 1, Subtraktion, Multiplikation, Division
+}
