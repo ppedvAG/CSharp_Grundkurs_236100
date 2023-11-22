572beee1dc7ac3d9bd72fe439ca1cb8ea1607c2b
ppedvLukasK <lukask@ppedv.de>
2023-11-22 15:01:53 +0100

    M008

---
+++ M008/AccessModifier.cs
@@ -0,0 +1,17 @@
+﻿namespace M008;
+
+internal class AccessModifier
+{
+	public int Alter { get; set; } //Kann überall gesehen werden (auch außerhalb vom Projekt)
+
+	private string Name { get; set; } //Kann nur innerhalb der Klasse gesehen werden
+
+	internal string Adresse { get; set; } //Kann überall gesehen werden (nur innerhalb des Projekts)
+
+
+	protected int ID { get; set; } //Kann nur innerhalb der Klasse gesehen werden, und auch in Unterklassen (auch außerhalb vom Projekt)
+
+	protected private string Description { get; set; } //Kann nur innerhalb der Klasse gesehen werden und auch in Unterklassen (nur innerhalb des Projekts)
+
+	protected internal string Haarfarbe { get; set; } //Kann überall gesehen werden (nur innerhalb des Projekts), aber auch in Unterklassen außerhalb des Projekts
+}
---
+++ M008/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace M008;
+
+public class Program
+{
+	static void Main(string[] args)
+	{
+		//Vererbung
+		//Ermöglicht das Definieren von einer Hierarchie von Klassen
+		//Oberklassen geben ihre Member an ihre Unterklassen weiter
+		//Grund: Polymorphismus
+
+		Mensch m = new Mensch("Max", "Deutsch");
+		m.Name = "Max"; //Mensch hat Name geerbt
+		m.GetHashCode(); //GetHashCode kommt von Object, weil Object die Oberklasse von allen Klassen ist
+		
+		//virtual
+		m.Bewegen(10); //Mensch bewegt sich um 10m
+
+		Katze k = new Katze("Nein");
+		k.Bewegen(20); //Lebewesen bewegt sich um 20m
+	}
+}
+
+/// <summary>
+/// Lebewesen ist die Oberklasse, und gibt alle Member von sich selbst nach unten weiter
+/// </summary>
+public class Lebewesen
+{
+	public string Name { get; set; } //Name wird an die Unterklassen weitergegeben
+
+	public Lebewesen(string name) //Konstruktor wird nach unten erzwungen
+	{
+		Name = name;
+	}
+
+	/// <summary>
+	/// virtual: Methode kann überschrieben werden, muss aber nicht
+	/// Wenn die Methode nicht überschrieben wurde, wird die Basisimplementation ausgeführt
+	/// </summary>
+	public virtual void Bewegen(int distanz)
+	{
+        Console.WriteLine($"Lebewesen bewegt sich um {distanz}m");
+    }
+}
+
+/// <summary>
+/// Mit : <Klasse> eine Vererbungshierarchie herstellen
+/// Hier: Mensch erbt von Lebewesen, Mensch ist ein Lebewesen
+/// </summary>
+public class Mensch : Lebewesen
+{
+	public string Sprache { get; set; }
+
+	/// <summary>
+	/// Strg + . -> Generate Constructor
+	/// </summary>
+	public Mensch(string name, string sprache) : base(name) //Mittels base eine Verkettung herstellen
+	{
+		Sprache = sprache; //Hier können extra Felder einfach hinzugefügt werden
+	}
+
+	/// <summary>
+	/// Mittels override kann eine Virtuelle Methode überschrieben werden
+	/// override tippen -> Abstand um Vorschläge zu bekommen
+	/// </summary>
+	public override void Bewegen(int distanz)
+	{
+        Console.WriteLine($"Mensch bewegt sich um {distanz}m");
+    }
+}
+
+/// <summary>
+/// sealed: Diese Klasse kann nicht weitervererbt werden
+/// </summary>
+public sealed class Katze : Lebewesen //Katze ist auch ein Lebewesen
+{
+	public Katze(string name) : base(name)
+	{
+
+	}
+}
