af1ebf132f188b61fa3f127e6c7ec6f1ffdb3926
ppedvLukasK <lukask@ppedv.de>
2023-11-22 13:32:31 +0100

    M000

--- M000/Program.cs
+++ M000/Program.cs
@@ -1,40 +1,14 @@
 ﻿while (true)
 {
-	Console.Write("Gib die erste Zahl ein: ");
-	double zahl1 = double.Parse(Console.ReadLine());
-
-	Console.Write("Gib die zweite Zahl ein: ");
-	double zahl2 = double.Parse(Console.ReadLine());
+	double zahl1 = ZahlEingabe("Gib die erste Zahl ein: ");
+	double zahl2 = ZahlEingabe("Gib die zweite Zahl ein: ");
 
 	foreach (Operation operation in Enum.GetValues<Operation>())
-	{
 		Console.WriteLine($"{(int) operation}: {operation}");
-	}
 
-	Console.Write("Gib eine Rechenoperation ein: ");
-	Operation op = Enum.Parse<Operation>(Console.ReadLine());
+	Operation op = RechenoperationEingabe();
 
-	switch (op)
-	{
-		case Operation.Addition:
-			Console.WriteLine($"Ergebnis: {zahl1 + zahl2}");
-			break;
-		case Operation.Subtraktion:
-			Console.WriteLine($"Ergebnis: {zahl1 - zahl2}");
-			break;
-		case Operation.Multiplikation:
-			Console.WriteLine($"Ergebnis: {zahl1 * zahl2}");
-			break;
-		case Operation.Division:
-			if (zahl2 != 0)
-				Console.WriteLine($"Ergebnis: {zahl1 / zahl2}");
-			else
-				Console.WriteLine("Teilung durch 0 ist nicht möglich!");
-			break;
-		default:
-			Console.WriteLine("Fehler");
-			break;
-	}
+	double ergebnis = Berechne(zahl1, zahl2, op);
 
     Console.WriteLine("Enter drücken zum Wiederholen");
 	if (Console.ReadKey(true).Key != ConsoleKey.Enter)
@@ -42,6 +16,54 @@
 	Console.Clear();
 }
 
+double Berechne(double zahl1, double zahl2, Operation op)
+{
+	switch (op)
+	{
+		case Operation.Addition:
+			Console.WriteLine($"{zahl1} + {zahl2} = {zahl1 + zahl2}");
+			return zahl1 + zahl2;
+		case Operation.Subtraktion:
+			Console.WriteLine($"{zahl1} - {zahl2} = {zahl1 - zahl2}");
+			return zahl1 - zahl2;
+		case Operation.Multiplikation:
+			Console.WriteLine($"{zahl1} * {zahl2} = {zahl1 * zahl2}");
+			return zahl1 * zahl2;
+		case Operation.Division:
+			if (zahl2 != 0)
+			{
+				Console.WriteLine($"{zahl1} / {zahl2} = {zahl1 / zahl2}");
+				return zahl1 / zahl2;
+			}
+			else
+			{
+				Console.WriteLine("Teilung durch 0 ist nicht möglich!");
+				return double.NaN;
+			}
+		default:
+			Console.WriteLine("Fehler");
+			return double.NaN;
+	}
+}
+
+double ZahlEingabe(string text)
+{
+	double result;
+	bool funktioniert;
+	while (true)
+	{
+        Console.WriteLine(text);
+		funktioniert = double.TryParse(Console.ReadLine(), out result);
+		if (funktioniert)
+			return result;
+	}
+}
+
+Operation RechenoperationEingabe()
+{
+	double eingabe = ZahlEingabe("Gib eine Rechenoperation ein: ");
+	return (Operation) eingabe;
+}
 
 enum Operation
 {
